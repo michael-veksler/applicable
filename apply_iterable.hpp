999855a0b163c4d0f40b3ac36f46729a8ba7652a
Michael Veksler <mickey.veksler@gmail.com>
2019-04-21 07:00:43 +0300

    Support C++14
    Issue #1 (works only with C++17 or newer)

--- apply_iterable.hpp
+++ apply_iterable.hpp
@@ -14,9 +14,9 @@ namespace app {
   template <class T>
   struct is_random_access_container<T,
       std::enable_if_t<
-	std::is_convertible_v<
+	std::is_convertible<
 	  typename std::iterator_traits<decltype(std::begin(std::declval<T>()))>::iterator_category,
-	  std::random_access_iterator_tag>>>
+	  std::random_access_iterator_tag>::value>>
     : std::true_type
   {};
   template <class T>
@@ -27,11 +27,20 @@ namespace app {
 	    >
   auto apply_n_impl(Func func, Tuple &&args, std::index_sequence<I1...>,
 		    RandomAccessible && elements, std::index_sequence<I2...>)
+    -> typename std::enable_if<std::is_rvalue_reference<decltype(elements)>::value,
+			       decltype(func(std::get<I1>(std::forward<Tuple>(args))..., std::move(elements[I2])...))>::type
   {
-    if constexpr (std::is_rvalue_reference_v<decltype(elements)>)
-      return func(std::get<I1>(std::forward<Tuple>(args))..., std::move(elements[I2])...);
-    else
-      return func(std::get<I1>(std::forward<Tuple>(args))..., elements[I2]...);
+    return func(std::get<I1>(std::forward<Tuple>(args))..., std::move(elements[I2])...);
+  }
+  template <unsigned size, class Func, class RandomAccessible,
+	    class Tuple, std::size_t... I1, std::size_t... I2
+	    >
+  auto apply_n_impl(Func func, Tuple &&args, std::index_sequence<I1...>,
+		    RandomAccessible && elements, std::index_sequence<I2...>)
+    -> typename std::enable_if<!std::is_rvalue_reference<decltype(elements)>::value,
+			       decltype(func(std::get<I1>(std::forward<Tuple>(args))..., std::move(elements[I2])...))>::type
+  {
+    return func(std::get<I1>(std::forward<Tuple>(args))..., elements[I2]...);
   }
   template <unsigned size, class Func, class RandomAccessible, class ...Args
 	    , class Enable = std::enable_if_t<
@@ -40,7 +49,7 @@ namespace app {
   auto apply_n(Func func, std::tuple<Args...> && args,
 	       RandomAccessible && elements)
   {
-    if (size != std::size(elements))
+    if (size != std::end(elements) - std::begin(elements))
       throw std::runtime_error("wrong number of elements");
     return apply_n_impl<size>(func,
 			      std::forward<std::tuple<Args...>>(args),
@@ -52,19 +61,32 @@ namespace app {
   template <unsigned min=0, unsigned max=100,
 	    class Func, class RandomAccessible, class ...Args,
 	    class Type = decltype(std::declval<RandomAccessible>()[1])
+            
+	    >
+  auto apply(Func func, std::tuple<Args...> && args,
+	     RandomAccessible && elements, typename std::enable_if<(min >= max)>::type * = nullptr)
+  {
+    if (std::end(elements) - std::begin(elements) == min) {
+      return apply_n<min>(func, std::forward<std::tuple<Args...>>(args),
+			  std::forward<RandomAccessible>(elements));
+    }
+    
+    throw std::runtime_error("Too many elements");
+  }
+  template <unsigned min=0, unsigned max=100,
+	    class Func, class RandomAccessible, class ...Args,
+	    class Type = decltype(std::declval<RandomAccessible>()[1]),
+            class = typename std::enable_if<(min < max)>::type
 	    >
   auto apply(Func func, std::tuple<Args...> && args,
 	     RandomAccessible && elements)
   {
-    if (std::size(elements) == min) {
+    if (std::end(elements) - std::begin(elements) == min) {
       return apply_n<min>(func, std::forward<std::tuple<Args...>>(args),
 			  std::forward<RandomAccessible>(elements));
     }
-    if constexpr(min < max)
-      return apply<min+1, max>(func, std::forward<std::tuple<Args...>>(args),
-			       std::forward<RandomAccessible>(elements));
-    else
-      throw std::runtime_error("Too many elements");
+    return apply<min+1, max>(func, std::forward<std::tuple<Args...>>(args),
+			     std::forward<RandomAccessible>(elements));
   }
 }
 #endif // APPLY_ITERABLE_HPP__
--- test.cpp
+++ test.cpp
@@ -3,14 +3,36 @@
 #include <apply_iterable.hpp>
 #include <memory>
 #include <array>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+
+void assertEqualStr(const char *a, const char *b, const char * msg = nullptr)
+{
+  if (strcmp(a, b) == 0)
+    return;
+  std::string msg_str;
+  if (msg) {
+    msg_str = msg;
+  } else {
+    std::ostringstream out;
+    out << "'" << a << "' != '" << b << "'";
+    msg_str = out.str();
+  }
+  std::cerr << msg_str << '\n';
+  throw std::runtime_error(msg_str);
+}
 
 int main()
 {
     std::vector<const char*> elements{"hello", "nice", "world"};
-    app::apply(std::printf, std::make_tuple("%s %s %s\n"), elements);
-    std::deque<int> int_elements{1,3,0};
-    app::apply<1,3>(std::fprintf, std::make_tuple(stdout, "%d,%d,%d\n"), int_elements);
     char buf[100];
+    app::apply(std::sprintf, std::make_tuple(buf, "%s %s %s"), elements);
+    assertEqualStr(buf, "hello nice world");
+    
+    std::deque<int> int_elements{1,3,0};
+    app::apply<1,3>(std::sprintf, std::make_tuple(buf, "%d,%d,%d"), int_elements);
+    assertEqualStr(buf, "1,3,0");
     
     const double nums[3] = { 1./9, 1./9, 1./9};
     bool had_exception = false;
@@ -23,8 +45,8 @@ int main()
       throw std::runtime_error("Expecting exception");
     }
       
-    app::apply_n<3>(std::sprintf, std::tuple{buf, "%.1f %.2f %.3f"}, nums);
-    puts(buf);
+    app::apply_n<3>(std::sprintf, std::make_tuple(buf, "%.1f %.2f %.3f"), nums);
+    assertEqualStr(buf, "0.1 0.11 0.111");
 
     std::array<std::unique_ptr<int>, 2> ptrs{std::make_unique<int>(1),
 				    std::make_unique<int>(2)};
@@ -40,4 +62,5 @@ int main()
     for (auto & x: ptrs) {
       if (x) throw std::runtime_error("should be deleted");
     }
+    std::cout << "OK\n";
 }
